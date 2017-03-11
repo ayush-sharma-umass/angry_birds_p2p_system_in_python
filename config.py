7a2950f0d084aa44e78930110f5a11266a2e8a48
Ayush Sharma <ayush.cs.umass@gmail.com>
2017-03-10 19:30:28 -0500

    added separate server files

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import constants.Constants as constants
 
 mode = constants.RuntimeConstants.MODE_TESTING
 
-serverHost1 = "128.119.243.147"
-serverPort1 = 9090
+serverHost1 = "" # Enter IP
+serverPort1 = 0000 # Enter port
 
-serverHost2 = "128.119.243.164"
-serverPort2 = 9090
+serverHost2 = "" # Enter IP
+serverPort2 = 9090 # Enter port
 
